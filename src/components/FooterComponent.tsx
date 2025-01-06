6898e5053160b18c355bbce072a392ba60e9e672
GarrixA <aphrogarrix10@gmail.com>
2025-01-06 11:36:35 +0200

    feat(project): initial project setup

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,9 @@
+const FooterComponent = () => {
+  return (
+    <div className="w-full h-12 bg-white text-black flex items-center px-12">
+      Footer
+    </div>
+  );
+};
+
+export default FooterComponent;
---
+++ src/components/NavbarComponent.tsx
@@ -0,0 +1,9 @@
+const NavbarComponent = () => {
+  return (
+    <div className="w-full h-12 bg-white text-black flex items-center px-12">
+      Navbar
+    </div>
+  );
+};
+
+export default NavbarComponent;
